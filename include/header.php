1b05ba79922457778d61a1b28e99a3b2d61c2bb4
SIHAAB <mirzashihab2@gmail.com>
2023-04-02 11:34:38 +0200

    init

---
+++ include/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <script type="text/javascript" src="../js/script.js"></script>
+    <script type="text/javascript" src="../js/jquery.js"></script>
+
+
+    <title>Global Wild Swimming And Camping</title>
+
+</head>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include_once "./include/header.php"?>
+
+<body>
+    <h1>jjakshdkjasdh</h1>
+</body>
