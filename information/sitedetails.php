3d6b35c75d8880cc7489fa8bddd99604566d2f18
SIHAAB <mirzashihab2@gmail.com>
2023-04-18 09:49:24 +0200

    details setup done

--- information/sitedetails.php
+++ information/sitedetails.php
@@ -2,18 +2,46 @@
 <?php include_once "../includes/navbar.php" ?>
 <?php include_once "../includes/db_connect.php" ?>
 
-<?php 
-
-if(isset($_GET['siteid'])){
-    echo $_GET['siteid'];
+<?php
+$siteid;
+if (isset($_GET['siteid'])) {
+    $siteid = addslashes($_GET['siteid']);
+}else{
+    die("<h1>site id needed</h1>");
 }
-
 ?>
 
 <body>
 
 
-    
+    <div class="sitedetails">
+
+        <div class="left">
+            <?php
+
+            $query = "SELECT * FROM CampingSites WHERE id = $siteid";
+
+            $result = $conn->query($query);
+
+            if ($result->num_rows > 0) {
+
+                
+             } else { ?>
+
+                <h1>Campsite with this id not found</h1>
+
+           <?php }
+            
+            ?>
+        </div>
+
+        <div class="right">
+
+        </div>
+
+        </div>
+
+
 </body>
 
 <?php include_once "../includes/footer.php" ?>
